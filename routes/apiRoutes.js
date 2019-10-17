47a0321caf666351038aae3f121e2ab1200a4297
Mahi Mani <m.mahishamani@gmail.com>
2019-10-17 11:59:24 -0700

    Merged front end and back end

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,26 @@
-var db = require("../models");
+// var db = require("../models");
 
-module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.render("index", { test: "test" })
-    })
+// module.exports = function (app) {
+//     app.get("/", function (req, res) {
+//         res.render("index", { test: "test" })
+//     })
 
-    app.put("/api/user/", function (req, res) {
-        db.users.findAll({
-            where: req.body
-        }).then(function(data) {
-            res.json(data)
-        }).catch(function(err) {
-            res.json(err)
-        })
-    })
+//     app.put("/api/user/", function (req, res) {
+//         db.users.findAll({
+//             where: req.body
+//         }).then(function(data) {
+//             res.json(data)
+//         }).catch(function(err) {
+//             res.json(err)
+//         })
+//     })
 
-    app.post("/api/user/", function (req, res) {
-        db.users.create(req.body).then(function (data) {
-            res.json(data);
-        }).catch(function (err) {
-            res.json(err)
-        })
-    })
-}
+//     app.post("/api/user/", function (req, res) {
+//         db.users.create(req.body).then(function (data) {
+//             res.json(data);
+//         }).catch(function (err) {
+//             res.json(err)
+//         })
+//     })
+// }
 
